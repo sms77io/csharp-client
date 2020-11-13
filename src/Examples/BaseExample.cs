9caa1329b06fcb3542059e1cc4f40cb4b2e7ffe9
André <matthiez@gmail.com>
2020-11-13 19:55:18 +0100

    removed examples

--- src/Examples/BaseExample.cs
+++
@@ -1,8 +0,0 @@
-using Sms77.Api;
-using Sms77.Tests;
-
-namespace Sms77.Examples {
-    class BaseExample {
-        protected static readonly Client Client = new Client(TestHelper.ApiKey);
-    }
-}
