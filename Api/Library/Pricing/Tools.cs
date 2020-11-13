f86ee374994de9585477c85d2983251447dea4d2
André <matthiez@gmail.com>
2020-11-13 17:39:11 +0100

    pricing: added defaults

---
+++ Api/Library/Pricing/Tools.cs
@@ -0,0 +1,8 @@
+namespace Sms77.Api.Library.Pricing {
+    public static class Tools {
+        public static PricingParams DefaultParams = new PricingParams {
+            Country = null,
+            Format = null
+        };
+    }
+}
