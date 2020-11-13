99e0cc275e7104ddd207f9c1cd959d80cbe12ec6
André <matthiez@gmail.com>
2020-11-13 17:45:22 +0100

    validate_for_voice: added defaults

---
+++ Api/Library/ValidateForVoice/Tools.cs
@@ -0,0 +1,7 @@
+namespace Sms77.Api.Library.ValidateForVoice {
+    public static class Tools {
+        public static ValidateForVoiceParams DefaultParams = new ValidateForVoiceParams {
+            Callback = null
+        };
+    }
+}
