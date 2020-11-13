2bee657eebc696a88c1d62ed12dc27e6c668f261
André <matthiez@gmail.com>
2020-11-13 17:46:44 +0100

    voice: added defaults

---
+++ Api/Library/Voice/Tools.cs
@@ -0,0 +1,8 @@
+namespace Sms77.Api.Library.Voice {
+    public static class Tools {
+        public static VoiceParams DefaultParams = new VoiceParams {
+            From = null,
+            Xml = false
+        };
+    }
+}
--- Api/Library/Voice/VoiceParams.cs
+++ Api/Library/Voice/VoiceParams.cs
@@ -4,7 +4,7 @@ namespace Sms77.Api.Library.Voice {
     public class VoiceParams {
         [JsonProperty("text")] public string Text { get; set; }
         [JsonProperty("to")] public string To { get; set; }
-        [JsonProperty("xml")] public bool Xml { get; set; }
-        [JsonProperty("from")] public string From { get; set; }
+        [JsonProperty("xml")] public bool? Xml { get; set; }
+        [JsonProperty("from")] public string? From { get; set; }
     }
 }
